423c1724a3c683dd0da59457f12c75217c07613f
wzapk <qhdtc5@gmail.com>
2019-10-27 09:24:29 +0800

    删除config.js

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export const settings = {
-  appName: window.SiteInfo.appName,
-  appTitle: window.SiteInfo.appTitle
-};
-export const authConfig = {
-  APPID: 'wx9116f47daa3bab34',
-  APPSECRET: '1e8729fe4ef9c9e71b141c9a87df5334'
-};
